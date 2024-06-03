35c7fba410fcf857d839af93d7d67e7c0e1876df
Nikesh pun magar <nikeshpun78@gmail.com>
2024-06-03 16:51:05 +0900

    Project initiation

---
+++ src/health/routes.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+const express = require('express');
+
+const controller = require('./controller/index');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  controller.getStatus(res);
+});
+
+module.exports = router;
